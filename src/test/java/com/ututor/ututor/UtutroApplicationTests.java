2bb0a2edee5e2379b50c2286a5ba559900f72c20
root <root@lhrotk-MACH-WX9>
2019-03-17 20:40:53 -0700

    User model

--- src/test/java/com/ututor/ututor/UtutroApplicationTests.java
+++ src/test/java/com/ututor/ututor/UtutroApplicationTests.java
@@ -2,15 +2,24 @@ package com.ututor.ututor;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.ututor.dao.UserDAO;
+import com.ututor.model.User;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class UtutroApplicationTests {
+	
+	@Autowired
+	UserDAO userDAO;
 
 	@Test
 	public void contextLoads() {
+		User user = userDAO.findByEmail("lhrotk@gmail.com");
+		System.out.println(user.toString());
 	}
 
 }
